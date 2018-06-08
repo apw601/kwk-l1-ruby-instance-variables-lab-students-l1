4bb7fb521d2c79a4f48177915429def171217183
apw601 <apw601@gmail.com>
2018-06-08 18:53:53 +0000

    automatically backed up by learn

--- nesteddatastructures.rb
+++ nesteddatastructures.rb
@@ -1,1 +1,11 @@
 #CREATE A HASH WITH NESTED ARRAYS OF YOUR GROUPS FAVORITE SONGS
+
+#CREATE A HASH
+
+group_name{
+  :kaityln => ["light em up", "the wolf"]
+  :alex => ["green light", "location"]
+  :bayleigh => ["want you back", "something to tell you"]
+}
+
+group_name
